07777bc1bdf30ff0e0504fc2960107b0894abe35
Amber <gynteee@yahoo.co.uk>
2023-01-16 09:53:10 +0000

    Fixed the glithch in printing students

--- copy_directory.rb
+++ copy_directory.rb
@@ -11,11 +11,12 @@
   #{name: "Joffrey Baratheon", cohort: :november},
 #  {name: "Norman Bates", cohort: :november}
 #]
-# instead of array or hash, we ask student for the name
+# an empty array accessible to all methods
+@students = []
+
 def student_input
   months = [:january, :february, :march,:april, :may, :june, :july, :august, :september, :october, :november, :december]
  # create an ampty array
-  students = []
   puts "Please enter the names of the students"
   puts "To finish, leave the name blank and press enter"
 # ask for user input on name
@@ -34,14 +35,13 @@ def student_input
     puts "And the hobby?"
     hobby = gets.chomp
     
-    students << {name: name, cohort: cohort, hobbies: hobby}
-    puts "Now we have #{students.count} students"
+    @students << {name: name, cohort: cohort, hobbies: hobby}
+    puts "Now we have #{@students.count} students"
     # get anohter name from the user
     puts "Please enter next student"
     name = gets.chomp
   end
-  # return the array of students
-  return students
+  # this method creats the list of students, no need to return it becasue students[] is declared outside of all methods. 
 end
 
 def print_header
@@ -49,52 +49,61 @@ def print_header
   puts "-------------".center(CENTER_SPACE)
 end
 
-def print(students)
-# while loop is printing student names while count is not equal number of students 
-  i = 0
-  while i != students.count do
-  puts "#{students[i][:name]} (#{students[i][:cohort]} cohort). #{students[i][:name]}'s favorite hobby is #{students[i][:hobbies]}".center(CENTER_SPACE)
-  i += 1
+def print_students_list
+# while loop is printing student names while count is not equal number of students
+  @students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort). #{student[:name]}'s favorite hobby is #{student[:hobbies]}".center(CENTER_SPACE)
   end
-  i += 1
 end
 
-def print_footer(students)
+def print_footer
   student_grammar = "student"
-  if students.length < 2
+  if @students.length < 2
     student_grammar = "student"
   else
     student_grammar = "students"
   end
-  puts "Overall, we have #{students.count} great #{student_grammar}.".center(CENTER_SPACE)
+  puts "Overall, we have #{@students.count} great #{student_grammar}.".center(CENTER_SPACE)
 end
 
 def interactive_menu
-  students = []
   # show user the list of sctins and get input
   loop do
-    puts "1. Input the students"
-    puts "2. Show the students"
-    puts "9. Exit"
-    selection = gets.chomp
-    case selection
-      when "1" 
-        students = student_input
-      when "2" 
-        print_header
-        print(students)
-        print_footer(students)
-      when "9" 
-        exit
+    print_menu
+    process(gets.chomp)
+  end
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "9. Exit"
+end
+
+def show_students
+  print_header
+  print_students_list
+  print_footer
+end
+
+def process(selection)
+  case selection
+    when "1" 
+     student_input
+    when "2" 
+      print_header
+      print_students_list
+      print_footer
+    when "9" 
+      exit
       else 
-        puts "I don't know what you meant, try again"
-    end
+      puts "I don't know what you meant, try again"
   end
 end
 
 CENTER_SPACE = 60
-#students = student_input
 interactive_menu
+#students = student_input
 #print_header
 #print(students)
 #print_footer(students)
